d6473a23360c4cea66edf032f9e6c9810f9a04c7
souravak <souravak211@gmail.com>
2023-03-04 19:34:41 +0530

    logo changes

--- script.js
+++ script.js
@@ -370,3 +370,21 @@ const observer = new IntersectionObserver((entries) => {
 hiddenElements.forEach(ele => {
     observer.observe(ele)
 })
+let mybutton = document.getElementById("gotoTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
